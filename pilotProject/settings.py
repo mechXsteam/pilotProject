c6553a3419cb8580337adaa0d746c1823a942043
Aditya Kumar <adityakumar_me20a8_36@dtu.ac.in>
2023-06-04 17:35:07 +0530

    ready for deployment, om ganeshayh namah

--- pilotProject/settings.py
+++ pilotProject/settings.py
@@ -126,7 +126,6 @@ DATABASES['default'].update(db_from_env)
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    "/var/www/static/",
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
